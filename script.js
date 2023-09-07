3bff89023a636acf732e3c1c7df548595649ffb1
danielm <lucascamargo.daniel@gmail.com>
2023-09-06 23:52:35 -0300

    i changed the whole logic, that was just bad

--- script.js
+++ script.js
@@ -1,83 +1,102 @@
-const body = document.querySelector("body");
-const head = document.querySelector("head");
+  const start = document.querySelector("#start");
+  const pause = document.querySelector("#pause");
+  const reset = document.querySelector("#reset");
 
-const holder = document.querySelector('div.holder');
+  const sec = document.querySelector("#second");
+  const min = document.querySelector("#minute");
+  const hour = document.querySelector("#hour");
+  
+  let countDown;
+  let setFlow = true; //countDown = true //countUp = false;
+  
+  let startTime = 0;
+  let elapsedTime = 0;
+  let paused = true;
+  let intervalId;
 
-const start = document.querySelector("#start");
-const pause = document.querySelector("#pause");
-const reset = document.querySelector('#reset');
-const scale = document.querySelector("body::before");
+function updateTime(){
 
-const sec = document.querySelector("#second");
-const min = document.querySelector("#minute");
-const hour = document.querySelector("#hour");
-
-const style = document.querySelector('style');
-let height;
-
-let compare;
-let capareGreen;
-
-let compareSec;
-let compareMin;
-let compareHour;
-
-let intervalId = false;
-
-const timer = () => {
-     intervalId = setInterval(() => {
-        if(+sec.value){
-            sec.value--;
-        }
-//////graph part>>>>>>>>>>>>>>>>>>>>>
-        compare = ( (+compareSec)  +  (+compareMin*60)  +  ((+compareHour*60)*60))
-        capareGreen = ((+sec.value)  +  (+min.value*60)  +  ((+hour.value*60)*60));
-
-        height = Math.abs((((capareGreen/compare)*100)-100));
-        style.innerHTML = `
-          body::before {
+    elapsedTime = Date.now() - startTime;
+    countDown = setFlow ?  sumDown - elapsedTime : elapsedTime;//it gonna be a if, else statement later because we need the graph
+    
+    if(countDown < 0){
+         paused = true;
+         clearInterval(intervalId);
+         window.alert("it's over");
         
-            background-color: green;
-            height: ${height}vh ;
-          }
-        `;
-//////graph part>>>>>>>>>>>>>>>>>>>>>
-
-        if( sec.value<10 ){
-            sec.value = "0" + sec.value;
-        }
-
-        if(sec.value == 0 ){
-            if( min.value != 0){
-                min.value--;
-                sec.value = 60;
-
-                if( min.value<10){
-                    min.value = "0" + min.value;
-                }
-               }
-               else{
-
-            if( hour.value != 0){
-                hour.value--;
-                min.value = 60;
-           }
-           else{
-
-            // do something when it's over///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-            clearInterval(intervalId);
-           }
-
-            if(hour.value<10){
-                hour.value = "0" + hour.value;
-            }
-        }
+        //countDown = Math.abs(countDown); we can work on that later put a - in front of the timer make it red, and bigerr
+        // and save that elapsed time when reache 0
     }
-}, 1000);
-};
 
-function validDigits(text){
+    sec.value = pad(Math.floor((countDown / 1000) % 60));
+    min.value = pad(Math.floor((countDown / (1000 * 60)) % 60));
+    hour.value = pad(Math.floor((countDown / (1000 * 60 * 60)) % 60));
+    
+
+    function pad(unit){
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
+}
+
+  start.addEventListener("click", () => {
+      if(paused){
+          paused = false;
+          startTime = Date.now();
+
+          if(setFlow){
+
+            if(sec.value == 0 && min.value == 0 && hour.value == 0){
+
+            sumDown = 30*1000*60;  
+            }
+            else{
+            sumDown = (sec.value*1000) + (min.value*1000*60) + (hour.value*1000*60*60);
+            }
+          }
+          intervalId = setInterval(updateTime, 1000);
+      }
+          start.style = "display: none;"
+          pause.style = "display: flex;"
+          reset.style = "display: flex;"
+  });
+
+  pause.addEventListener("click", () => {
+
+      if(!paused){
+        pause.innerHTML ="keep";
+        paused = true;
+        clearInterval(intervalId);
+      }
+      else{
+          pause.innerHTML ="pause";
+          paused = false;
+          startTime = Date.now() - elapsedTime;
+          intervalId = setInterval(updateTime, 1000);
+          }
+
+  });
+
+  reset.addEventListener("click", () => {
+      paused = true;
+      clearInterval(intervalId);
+
+      startTime = 0;
+      elapsedTime = 0;
+      currentTime = 0;
+
+      start.style = "display: flex;"
+      pause.style = "display: none;"
+      reset.style = "display: none;"
+      pause.innerHTML ="pause";
+      sec.value = "00";
+      hour.value = "00";
+      min.value = "00";
+  });
+
+
+
+
+  function validDigits(text){
     return text.replace(/^(?!([0-5]?[0-9]|60)$).*$/, "");
   }
 
@@ -88,66 +107,3 @@ function validDigits(text){
       e.target.value = updatedValue;
     });
   });
-
-start.addEventListener("click", () => {
-
-start.style = "display: none;"
-pause.style = "display: flex;"
-reset.style = "display: flex;"
-
-compareSec = +sec.value;
-compareMin = +min.value;
-compareHour = +hour.value;
-
-if(sec.value == 0 && min.value == 0 && hour.value == 0){
-   sec.value = "00";
-   min.value = 30;
-   hour.value = "00";
-    
-   compareSec = sec.value;
-   compareMin = min.value;
-   compareHour = hour.value;
-   }
-
-    if(!intervalId){
-    timer();
-    }
-});
-
-pause.addEventListener("click", ()=>{
-
-if(!intervalId){
-           
-  timer();
-  pause.innerHTML ="pause";
-}
-else{
-        
-    clearInterval(intervalId);
-    intervalId = false;
-    pause.innerHTML ="keep";
-    }
-})
-
-reset.addEventListener("click", () => {
-
-    start.style = "display: flex;"
-    pause.style = "display: none;"
-    reset.style = "display: none;"
-    
-    height = 100;
-    style.innerHTML = `
-    body::before {
-  
-    background-color: green;
-    height: ${height}vh ;
-    }
-  `;
-    pause.innerHTML ="pause";
-       sec.value = "00";
-       min.value = "00";
-       hour.value = "00";
-
-       clearInterval(intervalId);
-       intervalId = false;
-});
